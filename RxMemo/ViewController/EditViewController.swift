0ed54400c18b5ee9351f17957e4e5dfdecdea9f9
jooeun kim <jay311311@gmail.com>
2024-01-03 14:17:34 +0900

    feat. EditVC UI 추가 및 연결 storyboard 연결 - willAppear / willDisappear시 context뷰 활성화/비활성화 - 삭제, 저장 , contextView UI추가 및 연결 - viewModel Action binding  (with, Action)

--- RxMemo/ViewController/EditViewController.swift
+++ RxMemo/ViewController/EditViewController.swift
@@ -6,17 +6,55 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
+
 
 class EditViewController: UIViewController, ViewModelBindableType {
 
     var viewModel: EditViewModel!
     
+     
+    @IBOutlet weak var cancelBtn: UIBarButtonItem!
+    @IBOutlet weak var saveBtn: UIBarButtonItem!
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+
+        cancelBtn.rx.action = viewModel.cancelAction
+        
+        saveBtn.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        contentTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
     }
 }
