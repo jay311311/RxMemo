2653dfa2d0b6255d674b06f3d19c23a5d73434eb
jooeun kim <jay311311@gmail.com>
2024-01-03 14:00:48 +0900

    feat. EditViewModel 수정 - 메모 생성시 초기화 추가

--- RxMemo/ViewModel/EditViewModel.swift
+++ RxMemo/ViewModel/EditViewModel.swift
@@ -6,7 +6,48 @@
 //
 
 import Foundation
-class EditViewModel{
+import RxSwift
+import RxCocoa
+import Action
+
+class EditViewModel: CommonViewModel {
+    private let context: String?
     
+    var initialText: Driver<String?> {
+        return Observable.just(context).asDriver(onErrorJustReturn: nil)
+    }
     
+    let saveAction: Action<String, Void>
+    let cancelAction: CocoaAction
+    
+    init(
+        title: String,
+        context: String? = nil,
+        sceneCoordinator: SceneCoordinatorType,
+        storage: MemoStorageCRUD,
+        saveAction: Action<String, Void>? = nil,
+        cancelAction: CocoaAction? = nil
+    ) {
+        self.context = context
+        
+        self.saveAction = Action<String, Void> { input in
+            if let action = saveAction {
+                action.execute(input)
+            }
+            return sceneCoordinator.close(animated: true).asObservable().map{ _ in }
+        }
+        
+        self.cancelAction = CocoaAction { _ in
+            if let action = cancelAction {
+                action.execute(())
+            }
+            return sceneCoordinator.close(animated: true).asObservable().map{ _ in }
+        }
+        
+        super.init(
+            title: title,
+            sceneCoordinator: sceneCoordinator,
+            storage: storage
+        )
+    }
 }
